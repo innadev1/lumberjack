b06d25bdcb85cbf6420a62828a724e70d44a8d04
innadev1 <innad2201@gmail.com>
2017-08-18 12:56:12 +0300

    new

--- contacts.php
+++ contacts.php
@@ -6,6 +6,99 @@
 	</head>
 	<body>
 		<?php include 'assets/header.php'; ?>
+		
+		<div id="map_latvia">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2176.0142993381833!2d24.10916245184924!3d56.948556280796225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eecfd6ae6a8ee7%3A0x5201dc34e6cc9b53!2sRiharda+V%C4%81gnera+iela+11%2C+Centra+rajons%2C+R%C4%ABga%2C+LV-1050!5e0!3m2!1sru!2slv!4v1503046502723" width="600" 
+			height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+		
+		<div id="map_latvia_2"></div>
+		
+		<div id="map_estonia">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2028.76387625749!2d24.762843116355008!3d59.43701258169663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4692935e89169b51%3A0xebca4dd8672bbe25!2zUHJvbmtzaSAzLCAxMDEyNCBUYWxsaW5uLCDQrdGB0YLQvtC90LjRjw!5e0!3m2!1sru!2sru!4v1503047115221" width="600" 
+			height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+		
+		<div id="map_russia">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1998.9349913166286!2d30.248911716371083!3d59.933220881874256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x469630d1427f3f2b%3A0x7e406a0600f36d!2zMjkg0LvQuNC9LiDQktCw0YHQuNC70YzQtdCy0YHQutC-0LPQviDQvtGB0YLRgNC-0LLQsCwg0KHQsNC90LrRgi3Qn9C10YLQtdGA0LHRg9GA0LMsIDE5OTEwNg!5e0!3m2!1sru!2sru!4v1503047337531" width="600" 
+			height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+		
+		<div id="info">
+			<div>
+				<button class="first">MAIN OFFICE</button>
+				<table align="center">
+					<tr>
+						<th><h2>Adress:</h2></th>
+						<td><p>Riharda Vagnera iela 11,<br> Riga, Latvia</p></td>
+					</tr>
+					<tr>
+						<th><h2>Email:</h2></th>
+						<td><p>info@lumberjack.lv</p></td>
+					</tr>
+					<tr>
+						<th><h2>Tel.:</h2></th>
+						<td><p>+371 67 854 755</p></td>
+					</tr>
+				</table>
+				<button class="second">SHOW IN MAP</button>
+			</div>
+			<div>
+				<button class="first">MARKETING</button>
+				<table align="center">
+					<tr>
+						<th><h2>Adress:</h2></th>
+						<td><p>Riharda Vagnera iela 11,<br> Riga, Latvia</p></td>
+					</tr>
+					<tr>
+						<th><h2>Email:</h2></th>
+						<td><p>info@lumberjack.lv</p></td>
+					</tr>
+					<tr>
+						<th><h2>Tel.:</h2></th>
+						<td><p>+371 67 854 755</p></td>
+					</tr>
+				</table>
+				<button class="second">SHOW IN MAP</button>
+			</div>
+			<div>
+				<button class="first">INTERNET SHOP/RETAIL</button>
+				<table align="center">
+					<tr>
+						<th><h2>Adress:</h2></th>
+						<td><p>Riharda Vagnera iela 11,<br> Riga, Latvia</p></td>
+					</tr>
+					<tr>
+						<th><h2>Email:</h2></th>
+						<td><p>info@lumberjack.lv</p></td>
+					</tr>
+					<tr>
+						<th><h2>Tel.:</h2></th>
+						<td><p>+371 67 854 755</p></td>
+					</tr>
+				</table>
+				<button class="second">SHOW IN MAP</button>
+			</div>
+			<div>
+				<button class="first">FRANCHISE</button>
+				<table align="center">
+					<tr>
+						<th><h2>Adress:</h2></th>
+						<td><p>Riharda Vagnera iela 11,<br> Riga, Latvia</p></td>
+					</tr>
+					<tr>
+						<th><h2>Email:</h2></th>
+						<td><p>info@lumberjack.lv</p></td>
+					</tr>
+					<tr>
+						<th><h2>Tel.:</h2></th>
+						<td><p>+371 67 854 755</p></td>
+					</tr>
+				</table>
+				<button class="second">SHOW IN MAP</button>
+			</div>
+		</div>
+		
 		<?php include 'assets/footer.php'; ?>
 	</body>
 </html>

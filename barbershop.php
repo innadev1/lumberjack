726dda1f470ebd423ce553b54712838d4199411f
WillSylvan <silvis.silvestrs@gmail.com>
2017-09-26 16:24:48 +0300

    rando

--- barbershop.php
+++ barbershop.php
@@ -1,5 +1,5 @@
 <?php
-
+	include 'assets/lang.php';
 
 	$error_message_n = "";
 	$error_message_n2 = "";
@@ -202,15 +202,7 @@
 		
 		<div id="info_about_barbershop">
 			<p>
-				This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. 
-				Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis 
-				sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. 
-				Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. 
-				Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu 
-				ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. 
-				Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed 
-				ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum 
-				feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.
+			<?php echo $language[$lang]['barbtext'] ?>
 			</p>
 		</div>
 		
